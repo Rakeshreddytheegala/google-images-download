14935f647b7b9d95ab0b88744f4abe688d24980a
theegalarakeshreddy <trakeshreddy199@gmail.com>
2020-04-24 13:51:30 +0530

    made little changes to observe

--- setup.py
+++ setup.py
@@ -35,16 +35,5 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    keywords='google images download save filter color image-search image-dataset image-scrapper image-gallery terminal command-line',
-    packages=find_packages(exclude=['docs', 'tests*']),
-    include_package_data=True,
-    author='Hardik Vasa',
-    install_requires=install_requires,
-    dependency_links=dependency_links,
-    author_email='hnvasa@gmail.com',
-    entry_points={
-        'console_scripts': [
-            'googleimagesdownload = google_images_download.google_images_download:main'
-        ]},
 
 )
